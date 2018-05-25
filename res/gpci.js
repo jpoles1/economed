060ab17d9d2095c72685425e393a764298176fda
Jordan Poles <jpoles1@gmail.com>
2018-05-24 21:54:03 -0400

    Updates costs when locale changed

--- res/gpci.js
+++ res/gpci.js
@@ -1,9 +1,11 @@
+var gpciSetting;
 function calcCost(costEntry, gpciEntry){
     if(costEntry.cost){
         return costEntry.cost.toFixed(2)
     }
     else if(costEntry["CONV FACTOR"] && costEntry["WORK RVU"] && costEntry["FACILITY PE RVU"] && costEntry["MP RVU"]){
         cost = costEntry["CONV FACTOR"] * ((costEntry["WORK RVU"] * gpciEntry["PW GPCI"]) + (costEntry["FACILITY PE RVU"] * gpciEntry["PE GPCI"]) + (costEntry["MP RVU"] * gpciEntry["MP GPCI"]))
+        return cost.toFixed(2)
     }
     console.log("Error calculating cost for:", costEntry)
     return 0
@@ -21,7 +23,7 @@ function sumCosts() {
 
 function generateURL() {
     urlArray = []
-    $(".cost-input-box > .dropdown-input").each(function (_) {
+    $(".cost-input-box > .awesomplete > .dropdown-input").each(function (_) {
         costName = $(this).val();
         if(costName.length > 0){
             urlArray.push(costName)
@@ -31,6 +33,8 @@ function generateURL() {
     console.log(urlArray, urlString)
     console.log(encodeURI(urlString))
     window.location.hash = encodeURI(urlString)
+    baseURL = window.location.href.split('?')[0];
+    //history.pushState(null, null, baseURL + "?interventions="+encodeURIComponent(urlString));
     updateURLBox()
 }
 function updateURLBox(){
@@ -50,14 +54,20 @@ function loadURLConfig(costData) {
                     first = 0
                 }
                 $("#care-list").children(".cost-input").last().children(".cost-input-box").children("input").val(entry)
-                $("#care-list").children(".cost-input").last().children(".cost-value").children(".cost-number").html(costData[entry].cost.toFixed(2))
+                $("#care-list").children(".cost-input").last().children(".cost-value").children(".cost-number").html(calcCost(costData[entry], gpciSetting))
             }
         })
         sumCosts()
     }
 }
-
-function initializeLocaleInput(gpci_data) {
+function updateCosts(costData){
+    $(".cost-input-box > .awesomplete > .dropdown-input").each(function (_) {
+        costName = $(this).val();
+        $(this).parent().parent().parent().children(".cost-value").children(".cost-number").html(calcCost(costData[costName], gpciSetting))
+    })
+    sumCosts()
+}
+function initializeLocaleInput(gpci_data, costData) {
     console.log(gpci_data)
     var localeInput = $("#locale-selector")
     var btn = localeInput.siblings("button").first();
@@ -78,7 +88,8 @@ function initializeLocaleInput(gpci_data) {
         }
     });
     Awesomplete.$(localeInput[0]).addEventListener("awesomplete-selectcomplete", function () {
-        
+        gpciSetting = gpci_data[inputValue = $(this).val()]
+        updateCosts(costData)
     })
 }
 function initializeCostInput(costData) {
@@ -102,7 +113,7 @@ function initializeCostInput(costData) {
         });
         Awesomplete.$(entry).addEventListener("awesomplete-selectcomplete", function () {
             inputValue = $(this).val()
-            $(this).parents(".cost-input").children(".cost-value").children(".cost-number").html(costData[inputValue].cost.toFixed(2))
+            $(this).parents(".cost-input").children(".cost-value").children(".cost-number").html(calcCost(costData[inputValue], gpciSetting))
             sumCosts()
             generateURL()
         })
@@ -139,9 +150,10 @@ $(function () {
     $.getJSON("labs.json", function (labs_costs) {
         $.getJSON("gpci.json", function (gpci_data) {
             $.getJSON("rvu_costs.json", function (rvu_costs) {
+                gpciSetting = gpci_data[Object.keys(gpci_data)[0]]
                 costData = Object.assign({}, labs_costs, rvu_costs);
                 loadURLConfig(costData)
-                initializeLocaleInput(gpci_data)
+                initializeLocaleInput(gpci_data, costData)
                 initializeCostInput(costData)
                 activateDeleteButtons(costData)    
             })
